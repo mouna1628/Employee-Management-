f116f2712eeaa4e3a604cf2f631546dafdb8f2c6
Mouna M D <145093685+mouna1628@users.noreply.github.com>
2024-01-10 10:34:11 +0530

    Update EmployManagementSystem.java

--- EmployManagementSystem.java
+++ EmployManagementSystem.java
@@ -14,7 +14,7 @@ class MainMenu
     System.out.println("\t\t\t  EMPLOYEE MANAGEMENT SYSTEM");
     System.out.println("\t\t*******************************************");
     System.out.println("\t\t\t    --------------------");
-    System.out.println("\t\t\t     ~$ Abhinav Dubey");
+    System.out.println("\t\t\t     ~$ "Bablu");
     System.out.println("\t\t\t    --------------------");
     System.out.println("\n\nPress 1 : To Add an Employee Details");
     System.out.println("Press 2 : To See an Employee Details ");
@@ -159,7 +159,7 @@ class CodeExit
     System.out.println("\n*****************************************");
     System.out.println("$ cat Thank You For Using my Software :) ");
     System.out.println("*****************************************");
-    System.out.println("\t\t/~ <0d3d by Abhinav Dubey\n");
+    System.out.println("\t\t/~ <0d3d by Bablu\n");
     System.exit(0);
   }
 }
@@ -276,4 +276,4 @@ class EmployManagementSystem
   }
 }
 
-/****************************** CODED BY ABHINAV DUBEY ************************/
+/****************************** Bablu ************************/
